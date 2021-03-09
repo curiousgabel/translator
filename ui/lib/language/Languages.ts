7be60d2357ad2814f4e8494f41b32bc536aa6d6a
Mike Gabel <gabemic@gmail.com>
2021-03-08 22:25:16 -0600

    Added Bulgarian as a language option

--- ui/lib/language/Languages.ts
+++ ui/lib/language/Languages.ts
@@ -5,5 +5,6 @@ export const Languages: Record<string, Language> = {
     english: new Language('English', 'en'),
     spanish: new Language('Spanish', 'es'),
     french: new Language('French', 'fr'),
-    italian: new Language('Italian', 'it')
+    italian: new Language('Italian', 'it'),
+    bulgarian: new Language('Bulgarian', 'bg')
 } as const;
